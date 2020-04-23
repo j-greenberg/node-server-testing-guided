6ed522b81f07710d795cb6010a619b212f9cf779
j-greenberg <jgreenberg@runbox.com>
2020-04-23 14:19:09 -0400

    updated

---
+++ api/server.spec.js
@@ -0,0 +1,66 @@
+const request = require("supertest"); 
+
+const server = require('./server.js'); 
+const db = require('../data/dbConfig.js'); 
+
+describe('server', function() {
+    describe('/', function() {
+        it('should return 200 OK', function(){
+            // make a GET request to / endpoint on the server
+            return request(server)
+                .get('/')
+                .then(res => {
+                // assert that the HTTP status code is 200
+                    expect(res.status).toBe(200); 
+            })
+        })
+
+        describe("POST /hobbits", function () {
+            beforeEach(async () => {
+                await db('hobbits').truncate(); // empty the table and reset the id back to 1
+            })
+
+            it('"return 201 on success"', function(){
+                return request(server)
+                    .post("/hobbits")
+                    .send({ name: "gaffer"})
+                    .then(res => {
+                        expect(res.status).toBe(201); 
+                    })
+            }); 
+
+            it('"should return a message saying "Hobbit created successfully""', function(){
+                return request(server)
+                    .post("/hobbits")
+                    .send({ name: "gaffer" })
+                    .then(res => {
+                        expect(res.body.message).toBe("Hobbit created successfully"); 
+                    })
+            }); 
+
+            it("add the hobbit to the db", async function(){ 
+                const hobbitName = 'gaffer'; 
+
+                const existing = await db('hobbits').where({ name: hobbitName })
+                expect(existing).toHaveLength(0); 
+
+                await request(server)
+                    .post("/hobbits")
+                    .send({ name: hobbitName })
+                    .then(res => {
+                        expect(res.body.message).toBe("Hobbit created successfully"); 
+                    }); 
+
+                await request(server)
+                .post("/hobbits")
+                .send({ name: "sam" })
+                .then(res => {
+                    expect(res.body.message).toBe("Hobbit created successfully"); 
+                }); 
+
+                const inserted = await db('hobbits') //.where({ name: hobbitName })
+                expect(inserted).toHaveLength(2); 
+            }) ; 
+        })
+    })
+})
--- hobbits/hobbitsModel.js
+++ hobbits/hobbitsModel.js
@@ -9,6 +9,11 @@ module.exports = {
 };
 
 async function insert(hobbit) {
+  // add the hobbit to the db
+  // return 281 on success
+  // should return message saying "Hobbit created successfully"
+  return db("hobbits").insert(hobbit, "id"); 
+  
   return null;
 }
 
